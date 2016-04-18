39cfa126d29bc9e0eb00d4ee8ff7289d99706b24
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2016-04-18 10:25:47 -0400

    new scripts dir
    whitespace
    
    whitespace
    
    remove test file

---
+++ scripts/cities2geojson.js
@@ -0,0 +1,57 @@
+var cities = require('../cities.js')
+
+collection = {
+  type: "FeatureCollection",
+  features : []
+}
+
+cities.forEach(function(city){
+  collection.features.push({
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [city.coords[1],city.coords[0]]
+    },
+    "properties": {
+      "name": "Amsterdam",
+      "slug": "amsterdam",
+      "sort_order": 1000
+    }
+  })
+})
+
+console.log(JSON.stringify(collection));
+
+
+// SAMPLE GEOJSON SYNTAX
+// 
+// {
+//   "type": "FeatureCollection",
+//   "features": [
+//     {
+//       "type": "Feature",
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [4.8951679000000015,52.3702157]
+//       },
+//       "properties": {
+//         "name": "Amsterdam",
+//         "slug": "amsterdam",
+//         "sort_order": 1000
+//       }
+//     },
+
+//     {
+//       "type": "Feature",
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [-106.841831,39.1953106]
+//       },
+//       "properties": {
+//         "name": "Aspen",
+//         "slug": "aspen",
+//         "sort_order": 1000
+//       }
+//     }    
+//   ]
+// }
